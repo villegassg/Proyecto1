3d937386b529bcd89d1d3e6e3f047e5fa1bf8b65
tupatrona <145160387+tupatrona@users.noreply.github.com>
2023-10-22 23:21:17 -0600

    Update ConnectionListener.java

--- ConnectionListener.java
+++ ConnectionListener.java
@@ -1,5 +1,6 @@
 @FunctionalInterface
+    //Interfaz ConnectionListener
 public interface ConnectionListener {
-    
+    //metodo receivedMessage recibe como parametro dos objetos Connection y String
     public void receivedMessage(Connection connection, String message);
 }
