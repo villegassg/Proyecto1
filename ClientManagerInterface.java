9d8ed42e222178ef922a6578b883c2b08e1ae777
tupatrona <145160387+tupatrona@users.noreply.github.com>
2023-10-22 22:37:47 -0600

    Update ClientManagerInterface.java

--- ClientManagerInterface.java
+++ ClientManagerInterface.java
@@ -1,7 +1,9 @@
+//Interfaz del ClientManager
 public interface ClientManagerInterface {
     
+    //El método logIn recibe un parametro country del tipo String
     public void logIn(String country);
-
+   //El 
     public Client signIn();
 
     public Client signUp(String country);
