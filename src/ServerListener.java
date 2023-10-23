ba091abd1bde28c9ed34f8a07bfbce80cc627aa3
tupatrona <145160387+tupatrona@users.noreply.github.com>
2023-10-23 01:13:48 -0600

    Update ServerListener.java

--- src/ServerListener.java
+++ src/ServerListener.java
@@ -1,6 +1,7 @@
 @FunctionalInterface
+    //Interfaz de los oyentes¨ServerListener¨
 public interface ServerListener {
-
+//El método processMessage procesa los mensajes recibidos
     public void processMessage(String format, Object ... arguments);
     
 }
