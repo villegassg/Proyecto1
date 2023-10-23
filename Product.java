4fd67061ad8b62d5764e236ba421a763f1ee2cb5
tupatrona <145160387+tupatrona@users.noreply.github.com>
2023-10-23 00:29:10 -0600

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,21 +1,25 @@
+//Clase producto
 public class Product {
-
+//Creamos una variable name privada del tipo String
     private String name;
+//Creamos una variable barcode privada del tipo long
     private long barcode;
+//Creamos una variable privada department del tipo String
     private String department;
+//Creamos una variable privada price del tipo double
     private double price;
-
+//El método Product crea un constructor del Product el cual nos va dar el name, barcode, department and price del product
     public Product(String name, long barcode, String department, double price) {
         this.name = name;
         this.barcode = barcode;
         this.department = department;
         this.price = price;
     }
-
+//Regresa el name del producto
     public String getName() {
         return name;
     }
-
+//Regresa el barcode del producto
     public long getBarcode() {
         return barcode;
     }
@@ -23,11 +27,11 @@ public class Product {
     public String getDepartment() {
         return department;
     }
-
+//Regresa el precio del producto
     public double getPrice() {
         return price;
     }
-
+//El metodo toString regresa una cadena de texto de las caracteristicas del producto 
     @Override public String toString() {
         String n = String.format("Name: %s\n", name);
         String b = String.format("Barcode: %s\n", barcode);
@@ -36,7 +40,7 @@ public class Product {
 
         return n.concat(b).concat(d).concat(p);
     }
-
+//El metodo toString2 regresa una cadena de texto de las caracteristicas del productos
     public String toString2() {
         String n = String.format("Name: %s\t", name);
         String b = String.format("Barcode: %s\t", barcode);
@@ -45,7 +49,7 @@ public class Product {
 
         return n.concat(b).concat(d).concat(p);
     }
-
+//El metodo equals compara si dos productos don iguales.
     @Override public boolean equals(Object object) {
         if (!(object instanceof Product))
             return false;
