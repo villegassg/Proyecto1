f3a6222f98169421dd173f2e301b4a0897cbb312
tupatrona <145160387+tupatrona@users.noreply.github.com>
2023-10-22 22:32:50 -0600

    Update ClientConnectionInterface.java

--- ClientConnectionInterface.java
+++ ClientConnectionInterface.java
@@ -7,8 +7,9 @@ public interface ClientConnectionInterface {
     public void connect(Connection connection);
  //El metodo purchaseMode va hacer las acciones del modo de compra que desee el cliente
     public void purchaseMode();
-
+ //El metodo bankAccount1 va a recibir un parametro de conexión Connection llamado connection
     public void bankAccount1(Connection connection);
-
+    
+ //El metodo bankAccount2 va a recibir un parametro de conexión Connection llamado connection y un parametro product del tipo String
     public void bankAccount2(Connection connection, String product);
 }
