6f1dd0f252d7c351a7260166ba37872e16511534
tupatrona <145160387+tupatrona@users.noreply.github.com>
2023-10-22 23:40:14 -0600

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,22 +1,24 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
+//Clase Database contiene la base de datos de la tienda
 public class Database{
 
+    //Declaramos una variable privada del objeto que crea un arredlo Product
     private Product[][] products;
-
+//Clase de base de datos de la tienda virtual que implementa al Iterator
     private class DatabaseIterator implements Iterator<Product>{
         int i, j;
-
+//Inicializamos los arreglos de la base de datos de la tienda en 0
         private DatabaseIterator() {
             i = 0;
             j = 0;
         }
-
+//El metodo hasNext verifica si hay más elementos para recorrer la lista de los productos
         public boolean hasNext() {
             return i < products.length && j < products[i].length;
         }
-
+//El metodo next verifica si hay un siguiente elemento disponible en la coleccion llamando al
+//método hastnext
         public Product next() {
             if (hasNext()) { 
                 Product product = products[i][j++];
@@ -32,12 +34,12 @@ public class Database{
     int food = 0;
     int appliance = 0;
     int electronic = 0;
-
+//Inicializamos el arreglo 
     public Database() {
         products = new Product[3][15];
     }
 
-    //Barcode tiene qué cambiar al departamento.
+    //
     public void addProduct(Product product) {
         switch(product.getDepartment()) {
             case "ALIMENTICIO": 
@@ -52,7 +54,8 @@ public class Database{
         }
     }
 
-    //Barcode tiene qué cambiar al departamento.
+    //El método removeProduct organiza y almacena los productos en el arreglo segun su 
+    //tipo de departamento.
     public void removeProduct(Product product) {
         boolean removed = false;
         for (int i = 0 ; i < products.length ; i++)
@@ -66,7 +69,7 @@ public class Database{
                 System.out.println("Could not remove the product " + product.getName() +
                                     " because it is actually not in the database.\n");
     }
-
+//El método iterator regresa un nuevo itrdaor de la base de datos
     public Iterator<Product> iterator() {
         return new DatabaseIterator();
     }
