d1e2dbb0e48ddeb2b033ca4b98fccf069e9c76a0
tupatrona <145160387+tupatrona@users.noreply.github.com>
2023-10-22 21:48:07 -0600

    Update ClientConnectionInterface.java

--- ClientConnectionInterface.java
+++ ClientConnectionInterface.java
@@ -1,8 +1,10 @@
+//Interfaz de la conexión del cliente
 public interface ClientConnectionInterface {
-    
+
+    //El metodo purchase va a recibir un parametro de conexión Connection llamado connection
     public void purchase(Connection connection);
-
+ //El metodo connect va a recibir un parametro de conexión Connection llamado connection
     public void connect(Connection connection);
-
+ //El metodo purchaseMode va hacer las acciones del modo de compra que desee el cliente
     public void purchaseMode();
 }
